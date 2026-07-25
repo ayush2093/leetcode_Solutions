bf043c289a2085c9c323130a3f91d3217e553041
Ayush Singh <152475323+ayush2093@users.noreply.github.com>
2026-07-25 14:24:19 +0530

    Time: 2093 ms (19.31%), Space: 7.7 MB (90.05%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int found=false;
+        int sum=0;
+        for(int i=1; i<num; i++){
+            if(num%i==0){
+                sum=sum+i;
+            }
+        }
+        if(sum==num){
+            return true;
+        }
+        return false;
+    }
+};
