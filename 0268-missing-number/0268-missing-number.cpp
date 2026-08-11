cc4ddb254ea57f22cc60ccc83e249541f92ceb60
Ayush Singh <152475323+ayush2093@users.noreply.github.com>
2026-08-11 15:46:42 +0530

    Time: 0 ms (100%), Space: 21.9 MB (39.34%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        for(int i=0;i<=nums.size();i++){
-            bool found = false;
-
-            for(int j=0;j<nums.size();j++){
-                if(nums[j]==i){
-                    found=true;
-                    break;
-                }
+        sort(nums.begin(),nums.end());
+        int n = nums.size()-1;
+        for(int i=0;i<nums[n];i++){
+            if(nums[i]!=i){
+                return i;
             }
-    if(!found)return i;
         }
+        if(nums[n]==n)
+        return nums[n]+1;
+
         return -1;
     }
 };
