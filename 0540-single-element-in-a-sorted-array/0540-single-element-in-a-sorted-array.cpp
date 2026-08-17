1b2042bfb91ea313de5d63fdd08390f757eac14f
Ayush Singh <152475323+ayush2093@users.noreply.github.com>
2026-08-17 23:28:01 +0530

    Time: 0 ms (100%), Space: 26.2 MB (36.36%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int n = nums.size();
-        int start = 0, end = n - 1;
-
-        while(start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if((mid == 0 || nums[mid] != nums[mid-1]) &&
-               (mid == n-1 || nums[mid] != nums[mid+1]))
-                return nums[mid];
-
-            if(mid % 2 == 0) {
-
-                // EVEN → should pair with next
-                if(nums[mid] == nums[mid+1]) {
-                    start = mid + 1;       // single is RIGHT
+        int n=nums.size();
+        int start=0,end=n-1;
+        if(n==1)return nums[0];
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(mid==0 && nums[0]!=nums[1])return nums[0];
+            if(mid==n-1 && nums[n-1]!=nums[n-2])return nums[n-1];
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])return nums[mid];
+            if(mid%2==0){//rest elements are even
+                if(nums[mid]==nums[mid-1]){//single element in left
+                    end=mid-1;
                 }
-                else {
-                    end = mid - 1;         // single is LEFT
+                else{//single element in right
+                    start=mid+1;
                 }
-
             }
-            else {
-
-                // ODD → should pair with previous
-                if(nums[mid] == nums[mid-1]) {
-                    start = mid + 1;       // single is RIGHT
+            else{//rest elements are odd
+                if(nums[mid-1]==nums[mid]){//single element in right
+                        start=mid+1;
                 }
-                else {
-                    end = mid - 1;         // single is LEFT
+                else{//single element in left
+                        end=mid-1;
                 }
             }
         }
-
         return -1;
     }
 };
