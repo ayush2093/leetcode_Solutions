04b26e41b0c7abd17eb89479f49ace948c97e15c
Ayush Singh <152475323+ayush2093@users.noreply.github.com>
2026-08-25 23:23:15 +0530

    Time: 1 ms (24.27%), Space: 11.9 MB (71.74%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -3,17 +3,11 @@ public:
     bool uniqueOccurrences(vector<int>& nums) {
         int n=nums.size();
         unordered_map<int,int>freq;
-        for(auto c:nums){
-            freq[c]++;
-        }
+        for(auto c:nums)freq[c]++;
         unordered_map<int,int>ans;
-        for(auto c:freq){
-            ans[c.second]++;
-        }
+        for(auto c:freq)ans[c.second]++;
         for(auto c:ans){
-            if(c.second>1){
-                return false;
-            }
+            if(c.second>1)return false;
         }
         return true;
     }
