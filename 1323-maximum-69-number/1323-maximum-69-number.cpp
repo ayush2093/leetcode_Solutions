231dbf9e16221e10abec16bb8b74920a5a555a56
Ayush Singh <152475323+ayush2093@users.noreply.github.com>
2026-09-01 22:50:24 +0530

    Time: 0 ms (100%), Space: 8.1 MB (39.01%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,34 +1,13 @@
 class Solution {
 public:
-    int maximum69Number (int num) {
-        vector<int>ans;
-        int n=num;
-        while(n>0){
-            int rem=n%10;
-            ans.push_back(rem);
-            n=n/10;
-        }
-
-        int high=ans.size()-1,low=0;
-        while(low<=high){
-            swap(ans[high],ans[low]);
-            low++;
-            high--;
-        }
-        int m=0;
-        int ans1=num;
-        while(m<ans.size()){
-            if(ans[m]==6){
-            ans[m]=9;
-            int product=0;
-            for(int i=0;i<ans.size();i++){
-                product=product*10+ans[i];
+    int maximum69Number(int num) {
+        string s = to_string(num);
+        for(char &c : s) {
+            if(c == '6') {
+                c = '9';
+                break;
             }
-            ans1=max(product,ans1);
-            ans[m]=6;
-            }
-            m++;
         }
-        return ans1;
+        return stoi(s);
     }
 };
